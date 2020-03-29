b7ac6d5443300cedbe5fc4070d1e2f900b9713e9
unknown <eb.bakov@gmail.com>
2020-03-29 16:19:07 +0300

    test update

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -5,12 +5,26 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Recipe
+from core.models import Recipe, RecipeCategory, Ingredient
 
-from recipe.serializers import RecipeSerializer
+from recipe.serializers import RecipeSerializer, RecipeDetailSerializer
 
 
 RECIPES_URL = reverse('recipe:recipe-list')
+
+def sample_recipeCategory(user, name='Main course'):
+    """Create and return a sample recipeCategory"""
+    return RecipeCategory.objects.create(user=user, name=name)
+
+
+def sample_ingredient(user, name='Cinnamon'):
+    """Create and return a sample ingredient"""
+    return Ingredient.objects.create(user=user, name=name)
+
+
+def detail_url(recipe_id):
+    """Return recipe detail URL"""
+    return reverse('recipe:recipe-detail', args=[recipe_id])
 
 
 def sample_recipe(user, **params):
@@ -77,3 +91,104 @@ class PrivateRecipeApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        """Test viewing a recipe detail"""
+        recipe = sample_recipe(user=self.user)
+        recipe.recipeCategorys.add(sample_recipeCategory(user=self.user))
+        recipe.ingredients.add(sample_ingredient(user=self.user))
+
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_create_basic_recipe(self):
+        """Test creating recipe"""
+        payload = {
+            'name': 'Test recipe',
+            'cookTime': 30,
+            'price': 10.00,
+        }
+        res = self.client.post(RECIPES_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        for key in payload.keys():
+            self.assertEqual(payload[key], getattr(recipe, key))
+
+    def test_create_recipe_with_recipeCategorys(self):
+        """Test creating a recipe with recipeCategorys"""
+        recipeCategory1 = sample_recipeCategory(user=self.user, name='recipeCategory 1')
+        recipeCategory2 = sample_recipeCategory(user=self.user, name='recipeCategory 2')
+        payload = {
+            'name': 'Test recipe with two recipeCategorys',
+            'recipeCategorys': [recipeCategory1.id, recipeCategory2.id],
+            'cookTime': 30,
+            'price': 10.00
+        }
+        res = self.client.post(RECIPES_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        recipeCategorys = recipe.recipeCategorys.all()
+        self.assertEqual(recipeCategorys.count(), 2)
+        self.assertIn(recipeCategory1, recipeCategorys)
+        self.assertIn(recipeCategory2, recipeCategorys)
+
+    def test_create_recipe_with_ingredients(self):
+        """Test creating recipe with ingredients"""
+        ingredient1 = sample_ingredient(user=self.user, name='Ingredient 1')
+        ingredient2 = sample_ingredient(user=self.user, name='Ingredient 2')
+        payload = {
+            'name': 'Test recipe with ingredients',
+            'ingredients': [ingredient1.id, ingredient2.id],
+            'cookTime': 45,
+            'price': 15.00
+        }
+
+        res = self.client.post(RECIPES_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        recipe = Recipe.objects.get(id=res.data['id'])
+        ingredients = recipe.ingredients.all()
+        self.assertEqual(ingredients.count(), 2)
+        self.assertIn(ingredient1, ingredients)
+        self.assertIn(ingredient2, ingredients)
+
+    def test_partial_update_recipe(self):
+        """Test updating a recipe with patch"""
+        recipe = sample_recipe(user=self.user)
+        recipe.recipeCategorys.add(sample_recipeCategory(user=self.user))
+        new_recipeCategory = sample_recipeCategory(user=self.user, name='Curry')
+
+        payload = {'name': 'Chicken tikka', 'recipeCategorys': [new_recipeCategory.id]}
+        url = detail_url(recipe.id)
+        self.client.patch(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.name, payload['name'])
+        recipeCategorys = recipe.recipeCategorys.all()
+        self.assertEqual(len(recipeCategorys), 1)
+        self.assertIn(new_recipeCategory, recipeCategorys)
+
+    def test_full_update_recipe(self):
+        """Test updating a recipe with put"""
+        recipe = sample_recipe(user=self.user)
+        recipe.recipeCategorys.add(sample_recipeCategory(user=self.user))
+
+        payload = {
+                'name': 'Spaghetti carbonara',
+                'cookTime': 25,
+                'price': 5.00
+            }
+        url = detail_url(recipe.id)
+        self.client.put(url, payload)
+
+        recipe.refresh_from_db()
+        self.assertEqual(recipe.name, payload['name'])
+        self.assertEqual(recipe.cookTime, payload['cookTime'])
+        self.assertEqual(recipe.price, payload['price'])
+        recipeCategorys = recipe.recipeCategorys.all()
+        self.assertEqual(len(recipeCategorys), 0)    
